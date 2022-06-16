16e83fa5cfac84220ea11bf3c4260cdeec2fa101
henry <henrycanalesv@gmail.com>
2022-06-15 19:59:16 -0500

    clase api agregada

---
+++ AppEscritorio/Tools/Api.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppEscritorio.Tools
+{
+    internal class Api
+    {
+        public static async Task<TValue?> Get<TValue>(string url)
+        {
+            HttpClient client = new();
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            var json = await response.Content.ReadAsStringAsync();
+            var obj = JsonSerializer.Deserialize<TValue>(json);
+
+            return obj;
+        }
+
+        public static async Task<TResponse?> Post<TValue, TResponse>(string url, TValue obj)
+        {
+            var json = JsonSerializer.Serialize(obj);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpClient client = new();
+
+            var response = await client.PostAsync(url, data);
+
+            var resultJson = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<TResponse>(resultJson);
+
+            return result;
+        }
+    }
+}
--- AppEscritorio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ AppEscritorio/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace AppEscritorio {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AppEscritorio;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AppEscritorio;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
